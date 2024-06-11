528efe6d971aa3f485463891550f5f68440566bb
Papireddy <papireddyeppala903@gmail.com>
2024-06-11 14:21:12 +0530

    initial commit

---
+++ url.js
@@ -0,0 +1,3 @@
+// module.exports = `mongodb://localhost:27017`
+module.exports = `mongodb+srv://admin:WqlJq9Nu3w9DD23P@cluster0.zxzs2ky.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+// mongodb+srv://admin:WqlJq9Nu3w9DD23P@cluster0.zxzs2ky.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
